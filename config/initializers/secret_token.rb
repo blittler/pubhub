19fab86cc1edf75f3ac577eeda53bd996db1363a
Ben <ben.littler@gmail.com>
2014-04-29 22:10:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pubhub::Application.config.secret_key_base = 'd97de92c70fe6b3ef77c1883db2c40535a1430fed29743441c0e78d9b25a445a4073d4a712b232deacd32e8def405e39484ab66c3355ac7f10540ed82ee369ac'
